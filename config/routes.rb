80fe0827beb9bdfa1b12cc7b2aaa40abfa517a9d
ZacharyDagnall <zachary.dagnall@gmail.com>
2021-02-10 15:31:18 -0500

    commit tree 05b0296d4f8d58f4f2ee17ba0fcb45741beef79c

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root "application#start"
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  post "/login", to: "sessions#destroy"
+
+  get "/homepage", to: "sessions#homepage"  #, as: "homepage" #why can't i use a pathhelper here? threw errors
+  get "/secret", to: "secrets#show"
+
 end
